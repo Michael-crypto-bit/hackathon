9871268d012390ae239901fed9a52b1330b67234
Michael-crypto-bit <mbrimacombe28@students.stab.org>
2024-12-12 11:45:13 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,10 +7,21 @@ var release_year = getColumn(url, 5);
 var rating = getColumn(url, 6);
 var genre = getColumn(url, 7);
 
+function clearScreen(){
+    console.log("running clear function")
+    document.getElementById("input").innerHTML = "";
+    console.log("ending clear function")
+    }
+
+
 var Movies = [];
-for(var i = 0; i < type.length; i++){
+for(var i = 0; i<type.length; i++){
     if(type[i].includes("Movie")){
+        console.log("hi");
         Movies.push(names[i]);
+        
     }
 }
-console.log(Movies)
+console.log(Movies);
+
+
