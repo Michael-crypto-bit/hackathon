e4b128d2be7cd42990c6cae98b33526fa35a0f83
Michael-crypto-bit <mbrimacombe28@students.stab.org>
2024-12-12 11:24:58 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Politics/US%20Presidents.csv";
 var type = getColumn(url, 1);
-var name = getColumn(url, 2);
+var names = getColumn(url, 2);
 var country = getColumn(url, 3);
 var date_added = getColumn(url, 4);
 var release_year = getColumn(url, 5);
 var rating = getColumn(url, 6);
 var genre = getColumn(url, 7);
+
+var Movies = [];
+for(var i = 0; i < type.length; i++){
+    if(type[i].includes("Movie")){
+        Movies.push(names[i]);
+    }
+}
+console.log(Movies)
