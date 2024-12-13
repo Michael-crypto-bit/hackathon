3043552cccaa5cbeb384ca0df6bfea39d339084f
Michael-crypto-bit <mbrimacombe28@students.stab.org>
2024-12-13 13:04:28 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Politics/US%20Presidents.csv";
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Culture%20%26%20Entertainment/Netflix%20Content.csv";
 var type = getColumn(url, 1);
 var names = getColumn(url, 2);
 var country = getColumn(url, 3);
@@ -6,24 +6,30 @@ var date_added = getColumn(url, 4);
 var release_year = getColumn(url, 5);
 var rating = getColumn(url, 6);
 var genre = getColumn(url, 7);
-
+var input = document.getElementById("input").innerHTML;
 function clearScreen(){
     console.log("running clear function")
+    console.log(input)
     document.getElementById("input").value = "";
     console.log("ending clear function")
     }
+var genre1;
 
 
+
+function getMovies(){
 var Movies = [];
 for(var i = 0; i<type.length; i++){
     if(type[i].includes("Movie")){
-        console.log("hi");
         Movies.push(names[i]);
         
     }
 }
 console.log(Movies);
+}
+
 var matchingMovie = [];
+genre1 = document.getElementById("input").value
 function createList(genre1){
     for(var i = 0; i<type.length; i++){
         if(rating[i] == "R"){
@@ -32,4 +38,4 @@ function createList(genre1){
 
     }
 }
-console.log(jdkd, jdjdj)
+console.log(matchingMovie)
