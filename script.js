c03ffdeb4f508725ed07a20f0091f7a8666f02ca
Ned27 <eng27@students.stab.org>
2024-12-18 09:00:28 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,93 +1,32 @@
 var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Culture%20%26%20Entertainment/Netflix%20Content.csv";
-var type = getColumn(url, 1);
 var names = getColumn(url, 2);
-var country = getColumn(url, 3);
+var raw_release_year = getColumn(url, 5);
 var date_added = getColumn(url, 4);
-var release_year = getColumn(url, 5);
-var rating = getColumn(url, 6);
 var genre = getColumn(url, 7);
-var input = document.getElementById("input").innerHTML;
-function clearScreen(){
-    console.log("running clear function")
-    console.log(input)
-    document.getElementById("input").value = "";
-    console.log("ending clear function")
-    }
-var genre1;
-genre1 = document.getElementById("genre").value;
-console.log(genre1);
-
-
-function getMovies(){
-var Movies = [];
-var TvShows = [];
-for(var i = 0; i<type.length; i++){
-    if(type[i].includes("Movie")){
-        Movies.push(names[i]);
-        
-    }
-    else if(type[i].includes("TV Show")){
-        TvShows.push(names[i]);
-        
-    }
-}
-console.log(Movies);
-console.log("");
-console.log(TvShows);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var matchingMovie = [];
-function createList(genre1){
-    genre1 = document.getElementById("genre").value
-    for(var i = 0; i<type.length; i++){
-        if(rating[i] == "R"){
-            matchingMovie.push(names[i])
+var rating = getColumn(url, 6);
+var feedback = [];
+function searchMovies(){
+    feedback.pop(0)
+    feedback.pop(0)
+    feedback.pop(0)
+    feedback.pop(0)
+    feedback.pop(0)
+    feedback.pop(0)
+    feedback.pop(0)
+    feedback.pop(0)
+    feedback.pop(0)
+    feedback.pop(0)
+    var inputs = document.getElementById("input").value;
+    console.info(inputs);
+    for(var i = 0; i<names.length; i++){
+        if(names[i].toLowerCase().includes(inputs.toLowerCase())){
+            feedback.push(names[i], raw_release_year[i]);
+        // feedback.push(raw_release_year[i])        
         }
-
+    // else{
+    //     console.log("not found")
+    // }
     }
-}
-console.log(matchingMovie) 
-console.log(matchingMovie)
-var uniqueGenre = [];
-for(var i=0; i< genre.length; i++){
-    if(!uniqueGenre.includes(genre[i])){
-        uniqueGenre.push(genre[i])
-    }
-}
-console.log(uniqueGenre)
-
-var genreSelect = docment.getElementById("genre");
-for(var i = 0; i<type.length; i++){
-    // create a new option element
-    var el = document.createElement("option")
-    el.innerHTML = uniqueGenre[i]
-    el.value = uniqueGenre[i]
-    genreSelect.appendChild(el);
+    console.info(feedback);
+    document.getElementById("input").value = "";
 }
