3415291b1e09bb861648303deb39885adc759566
Michael-crypto-bit <mbrimacombe28@students.stab.org>
2024-12-18 09:22:29 -0500

    Add files via upload

--- search.js
+++ search.js
@@ -1,5 +1,14 @@
 var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Culture%20%26%20Entertainment/Netflix%20Content.csv";
 var names = getColumn(url, 2);
+var raw_release_year = getColumn(url, 5);
+var rating = getColumn(url, 6);
+var release_year = [];
+var i = 0;
+//
+while( i<raw_release_year.length){
+    release_year.push(parseInt(raw_release_year[i]));
+    i++;
+}
 var feedback = [];
 function searchMovies(){
 feedback.pop(0)
@@ -16,13 +25,14 @@ var inputs = document.getElementById("input").value;
 console.info(inputs);
 for(var i = 0; i<names.length; i++){
     if(names[i].toLowerCase().includes(inputs.toLowerCase())){
-        feedback.push(names[i]);
+        feedback.push("<br>" + names[i] + " " + release_year[i] + " " + rating[i]);
         
     }
     // else{
     //     console.log("not found")
-    // }
+    // }++
 }
 console.info(feedback);
 document.getElementById("input").value = "";
+document.getElementById("output").innerHTML = feedback;
 }
